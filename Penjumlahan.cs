b6ffa812d2602e169aaf3d544c63204e810d6f5a
haidar <haidarah134@gmail.com>
2024-03-23 08:23:08 +0700

    menambahkan class Penjumlahan denganmethod JumlahTigaAngka

---
+++ Penjumlahan.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul5_1302220152
+{
+    internal class Penjumlahan
+    {
+        public T JumlahTigaAngka<T>(T in1,T in2,T in3)
+        {
+            dynamic g = in1 as dynamic;
+            dynamic k = in2 as dynamic;
+            dynamic h = in3 as dynamic;
+            return g + k + h;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Penjumlahan pjm = new Penjumlahan();
+            // nim: 13 02 22 0152, tipe float
+            Console.WriteLine(pjm.JumlahTigaAngka((float)13.0, (float)02.0, (float)22.0));
         }
     }
 }
